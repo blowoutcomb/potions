a96f113ad44497f15e60b115345ca52ce586ee3a
blowoutcomb <1earthbound1@gmail.com>
2023-09-17 21:31:47 -0500

    done done

--- Assets/_myfiles/scripts/interactables/Cauldron.cs
+++ Assets/_myfiles/scripts/interactables/Cauldron.cs
@@ -1,57 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Cauldron : MonoBehaviour
 {
-    private bool isMushObj = false;
-    private bool isChopObj = false;
-    private bool isHerbObj = false;
+    private int pMushNum;
+    private int cHerb;
+    private int nHerb;
 
-    private int ingNum;
+    private int ingNum = 0;
     private int ingNumMax = 3;
 
     public GameObject pPot;
     public GameObject bPot;
-    public GameObject wPot;
+    public GameObject gPot;
+    public GameObject lPot;
+    public GameObject tPot;
+    public GameObject wrongPot;
 
     public GameObject potSpawn;
 
 
     private void OnTriggerEnter(Collider other)
     {
+        ingNum++;
         if (other.name.Contains("pMush"))
         {
-            ingNum++;
-            isMushObj = true;
+            pMushNum++;
         }
-
-
-        if (other.name.Contains("choppedHerb"))
+        else if (other.name.Contains("choppedHerb"))
         {
-            ingNum++;
-            isChopObj = true;
+            cHerb++;
         }
-
-
-        if (other.name.Contains("Herb"))
+        else if (other.name.Contains("Herb"))
         {
-            ingNum++;
-            isHerbObj = true;
+            nHerb++;
         }
-
         if(ingNum == ingNumMax)
         {
-           
+            CreatePot();
         }
+
+        Destroy(other.gameObject);
     }
 
-    private void OnTriggerExit(Collider other)
+    private void CreatePot()
     {
 
-       
+
+        if (pMushNum == 2 && cHerb == 1)
+        {
+            Instantiate(pPot);
+
+        }
+        else if (nHerb == 2 && pMushNum == 1)
+        {
+            Instantiate(lPot);
+        }
+
+        else if (cHerb == 3)
+        {
+            Instantiate(tPot);
+        }
+        else if (pMushNum == 3)
+        {
+            Instantiate(bPot);
+        }
+        else if (nHerb == 3)
+        {
+            Instantiate(gPot);
+        }
+        else
+        {
+            Instantiate(wrongPot);
+        }
+
+        ingNum = 0;
+        pMushNum = 0;
+        cHerb = 0;
+        nHerb = 0;
     }
-
-
-
 }
